2ff723870adf41b0961d1124266c9d2378aa43f8
Iordache Catalin <iordache.catalin1986@gmail.com>
2024-08-03 17:08:18 +0300

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 
+import { HomeComponent } from './Pages/home/home.component';
+
 export const routes: Routes = [
-    // {path: '',component: StartUpComponent,},
+    {path: '',component: HomeComponent,},
+    {path: 'home',component: HomeComponent,},
 ];
